000f014de5bdad40ad24ff51258598f619c418d8
Fhoxydeo <78915879+Fhoxydeo@users.noreply.github.com>
2021-08-30 13:39:20 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,21 @@
-#kuman
 import discord
 from discord.ext import commands
 
 #prefix
-client = commands.Bot(command_prefix = ' ! ')
+client = commands.Bot(command_prefix = '.')
 
 @client.event
 async def on_ready():
-    print('Bot is ready')
+    print('Berak Bos')
 
 @client.command()
-async def ping():
-    await client.say('Pong!')
+async def ping(ctx):
+    await ctx.send(f'Berak! {round(client.latency * 1000)}ms')
+
+@client.command()
+async def rules(ctx):
+    await ctx.send('https://media.discordapp.net/attachments/803233395021709312/880077552515964988/Screen_Shot_2019-03-17_at_12.16.56_PM.png?width=488&height=230')
+
 
 client.run('ODc0MTgxMzkwOTYxMzUyNzI1.YRDO3Q.5-tDgnuFGOhiyeX6-wtKzo2e1KE')
 
