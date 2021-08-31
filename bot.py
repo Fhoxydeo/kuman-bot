faaf96bdd278be2bfce7c4c2d4c941c3d0c61e17
Fhoxydeo <78915879+Fhoxydeo@users.noreply.github.com>
2021-08-31 20:42:51 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,8 +29,9 @@ async def berak(ctx):
 async def crot(ctx):
     await ctx.send('crat crot crat crot, brisik, bodoh, stop coli, stop menghabiskan sperma dalam tubuhmu biar kamu tidak mati lemas karena hipotensi, kunjungi website ini bila anda mengalami gejala hipotensi https://www.alodokter.com/hipotensi')
 
-@client.command(ctx)
-async def rushia
+@client.command()
+async def rushia(ctx):
+
     await ctx.send('bacot')
     
 #kumpulan aib
