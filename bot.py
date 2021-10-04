eb708bbf3eb5e04c09aedea1736fb4ec53bb46c1
Dedy Setiawan <dedyssg@gmail.com>
2021-10-04 21:15:44 +0700

    Update bot.py
    Snipe Command

--- bot.py
+++ bot.py
@@ -166,5 +166,26 @@ async def gaplok(self, ctx, user:discord.Member):
         em.set_image(url=url)
         await ctx.send(embed=em, files=[file])
 
+snipe_message_author = {}
+snipe_message_content = {}
+
+@client.event
+async def on_message_delete(message):
+     snipe_message_author[message.channel.id] = message.author
+     snipe_message_content[message.channel.id] = message.content
+     await sleep(60)
+     del snipe_message_author[message.channel.id]
+     del snipe_message_content[message.channel.id]
+
+@client.command(name = 'snipe')
+async def snipe(ctx):
+    channel = ctx.channel
+    try: #This piece of code is run if the bot finds anything in the dictionary
+        em = discord.Embed(name = f"Last deleted message in #{channel.name}", description = snipe_message_content[channel.id])
+        em.set_footer(text = f"This message was sent by {snipe_message_author[channel.id]}")
+        await ctx.send(embed = em)
+    except: #This piece of code is run if the bot doesn't find anything in the dictionary
+        await ctx.send(f"There are no recently deleted messages in #{channel.name}")
+
 #Token
-client.run('ODc0MTgxMzkwOTYxMzUyNzI1.YRDO3Q.c8bqeTkvsH01VoHUegq8oRXf3Ws')
+client.run(os.environ['DISCORD_TOKEN'])
